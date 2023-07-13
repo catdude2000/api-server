4c8f439f974a8c7537c5c0b74fe2e98ccaf819d0
Michael Pace <michael.g.pace@gmail.com>
2023-07-12 21:36:38 -0500

    added errors to serverjs and uncommented clothesmodel stuff

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@
 
 const express = require('express');
 const cors = require('cors');
+const handle500 = require('./error-handlers/500');
+const handle404 = require('./error-handlers/404');
 const foodRouter = require('./routes/food');
-// const clothesRouter = require('./routes/clothes')
+const clothesRouter = require('./routes/clothes');
 
 const app = express();
 
@@ -11,15 +13,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use(foodRouter);
-// app.use(clothesRouter)
+app.use(clothesRouter);
 app.get('/', (req, res, next) => {
   res.status(200).send('proof: LIFE!');
 });
 
-// app.use('*', (req, res, next) => {
-//   res.status(404).send('Not found');
-// });
-
+app.use('*', handle404);
+app.use(handle500);
 
 const start = (port) => app.listen (port, () => console.log('listening on port:', port));
 
