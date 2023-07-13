3cfcf4b6e71470514eea1f0d137055d514f3b7f4
Michael Pace <michael.g.pace@gmail.com>
2023-07-12 21:25:06 -0500

    food route done

--- src/routes/food.js
+++ src/routes/food.js
@@ -12,13 +12,22 @@ router.get('/food', async (req, res, next) => {
 
 router.get('/food/:id', async (req, res, next) => {
   let singleFood = await foodModel.findAll({where: {id: req.params.id}}); //where clause useful for update
-
   res.status(200).send(singleFood);
 });
 
 router.post('/food', async (req, res, next) => {
   let newFood = await foodModel.create(req.body);
   res.status(200).send(newFood);
+});
+
+router.put('/food/:id', async (req, res, next) => {
+  let updatedFood = await foodModel.findAll({where: {id: req.params.id}});
+  res.status(200).send(updatedFood);
+});
+
+router.delete('/food/:id', async (req, res, next) => {
+  let deletedFood = await foodModel.destroy({where: {id: req.params.id}});
+  res.status(204).json(deletedFood);
 });
 
 module.exports = router;
